44e6f67af7e9d906e46ed47bdf1c61c9ea0579bd
firay24 <fisan.onodaconan@gmail.com>
2024-04-01 16:33:17 +0700

    navbar and footer

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import LogoImg from "@/assets/logo.png";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <header className="shadow-sm">
+      <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
+        <Link href="/" className="flex items-center gap-3">
+          <Image src={LogoImg} alt="flow job logo" width={40} height={40} />
+          <span className="text-xl font-bold tracking-tight">Flow Jobs</span>
+        </Link>
+        <Button asChild>
+          <Link href="/jobs/new">Post a job</Link>
+        </Button>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
