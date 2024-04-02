c32c4891bf7dd922b6019168891295f0d1608680
firay24 <fisan.onodaconan@gmail.com>
2024-04-02 22:03:59 +0700

    create a new job

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "6qqvulupxkymxmw6.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
