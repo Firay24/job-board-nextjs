3b6c214ac6bc2eb89d501af0bac1f4aa60354ad6
Hatakee Fira <fisan.onodaconan@gmail.com>
2024-04-05 00:04:48 +0700

    detail job and markdown style

--- src/components/JobPage.tsx
+++ src/components/JobPage.tsx
@@ -1,6 +1,10 @@
+import { formatMoney } from "@/lib/utils";
 import { Job } from "@prisma/client";
+import { Banknote, Briefcase, Globe2, MapPin } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Markdown from "./Markdown";
 
 interface JobPageProps {
   job: Job;
@@ -31,8 +35,43 @@ export default function JobPage({
             className="rounded-xl"
           />
         )}
+        <div>
+          <div>
+            <h1>{title}</h1>
+            <p className="font-semibold">
+              {applicationUrl ? (
+                <Link
+                  href={new URL(applicationUrl).origin}
+                  className="text-green-500"
+                >
+                  {companyName}
+                </Link>
+              ) : (
+                <span>{companyName}</span>
+              )}
+            </p>
+          </div>
+          <div className="text-muted-foreground">
+            <p className="flex items-center gap-1.5">
+              <Briefcase size={16} className="shrink-0" />
+              {type}
+            </p>
+            <p className="flex items-center gap-1.5">
+              <MapPin size={16} className="shrink-0" />
+              {locationType}
+            </p>
+            <p className="flex items-center gap-1.5">
+              <Globe2 size={16} className="shrink-0" />
+              {location || "Worldwide"}
+            </p>
+            <p className="flex items-center gap-1.5">
+              <Banknote size={16} className="shrink-0" />
+              {formatMoney(salary)}
+            </p>
+          </div>
+        </div>
       </div>
-      <div></div>
+      <div>{description && <Markdown>{description}</Markdown>}</div>
     </section>
   );
 }
